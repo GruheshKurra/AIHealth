7f336ef69618ee9889bf690439f289d1ab74f031
Kurra Gruhesh Sri Sai Karthik <106949584+GruheshKurra@users.noreply.github.com>
2025-02-13 12:55:42 +0530

    Update VegetablePricePrediction.jsx

--- src/pages/VegetablePricePrediction.jsx
+++ src/pages/VegetablePricePrediction.jsx
@@ -53,7 +53,7 @@ const VegetablePricePrediction = () => {
   const getInitialOptions = async () => {
     try {
       const response = await fetch(
-        'https://generativelanguage.googleapis.com/v1/models/gemini-1.0-pro:generateContent?key=AIzaSyAPAxDuNVKDzmXwG6K0V9kxhGrJGnvjjKY',
+        'https://generativelanguage.googleapis.com/v1/models/gemini-1.0-pro:generateContent?key=AIzaSyBsVQd8YdS1rTIXKhucs321T6Rzc2ZpD-Q',
         {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
@@ -85,7 +85,7 @@ const VegetablePricePrediction = () => {
 
   const getPredictions = async () => {
     const response = await fetch(
-      'https://generativelanguage.googleapis.com/v1/models/gemini-1.0-pro:generateContent?key=AIzaSyAPAxDuNVKDzmXwG6K0V9kxhGrJGnvjjKY',
+      'https://generativelanguage.googleapis.com/v1/models/gemini-1.0-pro:generateContent?key=AIzaSyBsVQd8YdS1rTIXKhucs321T6Rzc2ZpD-Q',
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
