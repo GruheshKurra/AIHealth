e34e714b287a6d52715616dada67fe582b928615
Kurra Gruhesh Sri Sai Karthik <106949584+GruheshKurra@users.noreply.github.com>
2025-02-13 08:45:22 +0530

    Update PlantAnalysis.jsx

--- src/components/PlantAnalysis.jsx
+++ src/components/PlantAnalysis.jsx
@@ -431,7 +431,7 @@ const PlantAnalysis = () => {
           setProgress(60);
 
           const response = await fetch(
-            'https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=AIzaSyAD1IsW4iMD1HnZjP1MYHb5Qb8CqwOLFXs',
+            'https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=AIzaSyAPAxDuNVKDzmXwG6K0V9kxhGrJGnvjjKY',
             {
               method: 'POST',
               headers: { 'Content-Type': 'application/json' },
