d0a6522ffcf71bbfc6b46cb32547b9cf61764610
Kurra Gruhesh Sri Sai Karthik <106949584+GruheshKurra@users.noreply.github.com>
2025-02-13 08:46:24 +0530

    Update SoilAnalysis.jsx

--- src/pages/SoilAnalysis.jsx
+++ src/pages/SoilAnalysis.jsx
@@ -360,7 +360,7 @@ const SoilAnalysis = () => {
       }`;
 
       const response = await fetch(
-        'https://generativelanguage.googleapis.com/v1/models/gemini-1.0-pro:generateContent?key=AIzaSyAD1IsW4iMD1HnZjP1MYHb5Qb8CqwOLFXs',
+        'https://generativelanguage.googleapis.com/v1/models/gemini-1.0-pro:generateContent?key=AIzaSyAPAxDuNVKDzmXwG6K0V9kxhGrJGnvjjKY',
         {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
